cde98ebf6e46a5237a39f9a91fd6499744bbbd10
Dhruvpopa <202151050@iiitvadodara.ac.in>
2024-07-30 21:14:52 +0530

    Muzzle Flash

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,6 +8,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 30f;
+    [SerializeField] ParticleSystem muzzleFlash;
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
@@ -18,8 +19,19 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRaycast()
+    {
         RaycastHit hit;
-        if(Physics.Raycast(FPCamera.transform.position,FPCamera.transform.forward, out hit, range))
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
 
@@ -32,6 +44,5 @@ public class Weapon : MonoBehaviour
         {
             return;
         }
-        
     }
 }
