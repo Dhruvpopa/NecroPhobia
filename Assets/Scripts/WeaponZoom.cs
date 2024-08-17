30cafe21b4d2d7637134ea31365e01bf1a8d01ec
Dhruvpopa <202151050@iiitvadodara.ac.in>
2024-08-18 04:12:05 +0530

    ZoomInSensitivity

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour
 {
     [SerializeField] Camera fpsCamera;
     [SerializeField] float zoomedOutFOV = 60f;
     [SerializeField] float zoomedInFOV = 20f;
+    [SerializeField] float zoomedOutSensitivity = 2f;
+    [SerializeField] float zoomedInSensitivity = 0.5f;
+
+    RigidbodyFirstPersonController fpsController;
 
     bool zoomedInToggle = false;
+
+    public void Start()
+    {
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
+    }
     private void Update()
     {
         if (Input.GetMouseButtonDown(1))
@@ -17,11 +27,15 @@ public class WeaponZoom : MonoBehaviour
             {
                 zoomedInToggle = true;
                 fpsCamera.fieldOfView = zoomedInFOV;
+                fpsController.mouseLook.XSensitivity = zoomedInSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomedInSensitivity;
             }
             else
             {
                 zoomedInToggle = false;
                 fpsCamera.fieldOfView = zoomedOutFOV;
+                fpsController.mouseLook.XSensitivity = zoomedOutSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomedOutSensitivity;
             }
         }
     }
